8a4e7a1a7f34c1599fd5575cb38c097e6d44e4c5
Kingsley Effiong <122705122+kingsfavy@users.noreply.github.com>
2025-10-04 01:24:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const users = [];
 
 // Signup API
-app.post("/api/signup", (req, res) => {
+app.post("/", (req, res) => {
   const { username, password } = req.body;
 
   if (users.find(u => u.username === username)) {
@@ -33,7 +33,7 @@ app.post("/api/signup", (req, res) => {
 });
 
 // Login API
-app.post("/api/login", (req, res) => {
+app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   const user = users.find(u => u.username === username && u.password === password);
