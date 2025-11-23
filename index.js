6045544599729bd3e7ba0e5301c121030089a3fa
Kingsley Effiong <122705122+kingsfavy@users.noreply.github.com>
2025-11-23 02:23:25 +0100

    Fix file path concatenation in response methods

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ app.post("/login", (req, res) => {
 
 // Page routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, + "index.html"));
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "login.html"));
+  res.sendFile(path.join(__dirname, + "login.html"));
 });
 
 // Start server
